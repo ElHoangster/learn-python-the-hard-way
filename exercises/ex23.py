93ddb8ecea940520b17609e59c9564aa42fc8d10
Brian Hoang <brian@Brians-MacBook-Pro.local>
2019-08-31 17:01:49 -0700

    redid ex23.py

--- exercises/ex23.py
+++ exercises/ex23.py
@@ -1,11 +1,14 @@
 import sys
 script, input_encoding, error = sys.argv
 
+
 def main(language_file, encoding, errors):
     line = language_file.readline()
+
     if line:
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
+
 
 def print_line(line, encoding, errors):
     next_lang = line.strip()
